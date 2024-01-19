7b580b1bd8303fdac642e54b2dcd43d1340c4c04
Utku Şener <121038395+utksnr@users.noreply.github.com>
2024-01-19 15:08:01 +0300

    Add files via upload

---
+++ age_detection/age_detection.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+from deepface import DeepFace
+
+clf = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+cam = cv2.VideoCapture(0)
+
+while True:
+    _,frame = cam.read()
+    gray_cam = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = clf.detectMultiScale(gray_cam,scaleFactor=1.2,minNeighbors=5,minSize=(30,30),flags=cv2.CASCADE_SCALE_IMAGE)
+    res = DeepFace.analyze(frame, actions=("age",),enforce_detection=False)
+    age = str(res[0]["age"])
+
+
+    for (x,y,w,h) in faces:
+        cv2.putText(frame,text=age, org=(x,y), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=1, color=(255,255,0))
+        cv2.rectangle(frame,(x,y), (x+w, y+h), (255,255,0),2)
+
+    cv2.imshow("",frame)
+    if cv2.waitKey(20) == ord("q"):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
